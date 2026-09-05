2486b992eaedb3188ab090b32ce1076451efb79b
Your Name <nyikogivenvumani@gmail.com>
2026-09-05 18:42:19 +0200

    Nyiko just added

--- RoyalDeshBord/src/App.jsx
+++ RoyalDeshBord/src/App.jsx
@@ -10,6 +10,7 @@ import ContactPage from './welcome/ContactPage'
 // AfterLogin directory pages (matching exact file names and casing)
 import LoginPage from './afterLogin/loginPage'
 import DashPage from './afterLogin/DashPage'
+import DashPage from './afterLogin/DashPage'
 
 export default function App() {
   return (
@@ -19,6 +20,7 @@ export default function App() {
         <Route path="/about" element={<AboutUs />} />
         <Route path="/products" element={<Products />} />
         <Route path="/contact" element={<ContactPage />} />
+        <Route path="/dashboard" element={<DashPage />} />
         
         {/* Authenticated / Portal Routes */}
         <Route path="/login" element={<LoginPage />} />
