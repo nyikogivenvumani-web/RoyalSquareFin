57dec576631f223c03c604871aae912e4565d1ce
Your Name <nyikogivenvumani@gmail.com>
2026-09-05 12:49:41 +0200

    Hre are some pages

--- RoyalDeshBord/src/App.jsx
+++ RoyalDeshBord/src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
 
+import heroImg from './assets/hero.png'
+import './App.css'
---
+++ RoyalDeshBord/src/welcome/HomePage.jsx
@@ -0,0 +1,7 @@
+import React from react
+
+function HomePage() {
+    return (
+        <div>Welcom to Royal Financal Investments</div>
+    )
+}
